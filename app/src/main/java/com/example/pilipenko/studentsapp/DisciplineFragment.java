af9ba2ab323e7ad87be587b51870aa2e85d48e2c
pilipenko <pilin94@gmail.com>
2016-08-17 18:04:14 +0300

    Disciplines list page 5_1 in progress. Add item_discipline

--- app/src/main/java/com/example/pilipenko/studentsapp/DisciplineFragment.java
+++ app/src/main/java/com/example/pilipenko/studentsapp/DisciplineFragment.java
@@ -3,17 +3,22 @@ package com.example.pilipenko.studentsapp;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class DisciplineFragment extends Fragment {
 
     private ToolbarI mToolbarActivity;
+
+    private ImageButton mNavigatorPriorImageButton;
+    private ImageButton mNavigatorNextImageButton;
 
     public static DisciplineFragment newInstance() {
 
@@ -22,6 +27,28 @@ public class DisciplineFragment extends Fragment {
         DisciplineFragment fragment = new DisciplineFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_discipline, container, false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.fragment_discipline_toolbar);
+        mToolbarActivity.useToolbar(toolbar);
+
+        NavigatorButtonOnClickListener onClickListener = new NavigatorButtonOnClickListener();
+        mNavigatorPriorImageButton = (ImageButton) view.findViewById(R.id.toolbar_navigator_btn_prior);
+        mNavigatorNextImageButton = (ImageButton) view.findViewById(R.id.toolbar_navigator_btn_next);
+        mNavigatorPriorImageButton.setOnClickListener(onClickListener);
+        mNavigatorNextImageButton.setOnClickListener(onClickListener);
+
+        return view;
     }
 
     @Override
@@ -37,18 +64,35 @@ public class DisciplineFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.discipline, menu);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_discipline, container, false);
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.fragment_discipline_toolbar);
-        mToolbarActivity.useToolbar(toolbar);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.discipline_menu_item_search:
+                Toast.makeText(getActivity(), "Поиск!", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-        return view;
+    private class NavigatorButtonOnClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()) {
+                case R.id.toolbar_navigator_btn_prior:
+                    Toast.makeText(getActivity(), "Prior", Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.toolbar_navigator_btn_next:
+                    Toast.makeText(getActivity(), "Next", Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
     }
 
     public interface ToolbarI {
