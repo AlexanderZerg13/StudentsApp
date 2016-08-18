e7ed038a05e0f0ae0d8ac3b3fae4669880d96b03
pilipenko <pilin94@gmail.com>
2016-08-18 17:06:22 +0300

    Discipline describe page (5_2) functional completed (5_2_1)

--- app/src/main/java/com/example/pilipenko/studentsapp/DisciplineDescribeFragment.java
+++ app/src/main/java/com/example/pilipenko/studentsapp/DisciplineDescribeFragment.java
@@ -1,15 +1,27 @@
 package com.example.pilipenko.studentsapp;
 
+import android.content.res.Resources;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.pilipenko.studentsapp.com.example.pilipenko.data.Discipline;
 import com.example.pilipenko.studentsapp.com.example.pilipenko.data.StaticData;
+import com.example.pilipenko.studentsapp.com.example.pilipenko.data.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class DisciplineDescribeFragment extends Fragment {
 
@@ -17,6 +29,11 @@ public class DisciplineDescribeFragment extends Fragment {
     private static final String KEY_BUNDLE_DISCIPLINE = "BUNDLE_DISCIPLINE";
 
     private Discipline mDiscipline;
+    private List<Teacher> mTeacherList;
+
+    private Button mMoreButton;
+    private TextView mDisciplineNameTextView;
+    private LinearLayout mTeachersLinearLayout;
 
     public static DisciplineDescribeFragment newInstance(int idSemester, int idDiscipline) {
 
@@ -36,6 +53,15 @@ public class DisciplineDescribeFragment extends Fragment {
         int idDiscipline = getArguments().getInt(KEY_BUNDLE_DISCIPLINE);
 
         mDiscipline = StaticData.sSemesters.get(idSemester).getDisciplineList().get(idDiscipline);
+        Random r = new Random();
+        if (r.nextBoolean()) {
+            mTeacherList = StaticData.sTeachers;
+            mTeacherList.get(0).setName(mDiscipline.getTeacherName());
+        } else {
+            mTeacherList = new ArrayList<>();
+            mTeacherList.add(new Teacher(mDiscipline.getTeacherName(), StaticData.sTeachers.get(0).getPost()));
+        }
+
     }
 
     @Override
@@ -48,6 +74,53 @@ public class DisciplineDescribeFragment extends Fragment {
         ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         ((AppCompatActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
 
+
+        mMoreButton = (Button) view.findViewById(R.id.fragment_discipline_describe_btn_more);
+        mDisciplineNameTextView = (TextView) view.findViewById(R.id.fragment_discipline_describe_tv_name);
+        mTeachersLinearLayout = (LinearLayout) view.findViewById(R.id.fragment_discipline_describe_ll_teachers);
+
+        mDisciplineNameTextView.setText(mDiscipline.getName());
+
+        setupTeachers();
+
         return view;
+    }
+
+    private void setupTeachers() {
+        Teacher teacher = mTeacherList.get(0);
+        mTeachersLinearLayout.addView(getTeacherView(teacher, null));
+
+        if (mTeacherList.size() == 1) {
+            mMoreButton.setVisibility(View.INVISIBLE);
+        } else {
+            mMoreButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mMoreButton.setVisibility(View.INVISIBLE);
+                    LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+                    Resources r = getResources();
+                    for (int i = 1; i < mTeacherList.size(); i++) {
+                        LinearLayout.LayoutParams margin = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                        int topMargin = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 12, r.getDisplayMetrics());
+                        margin.setMargins(0,topMargin, 0, 0);
+                        Log.i("TAG", "onClick: " + topMargin);
+                        mTeachersLinearLayout.addView(getTeacherView(mTeacherList.get(i), layoutInflater), margin);
+                    }
+                }
+            });
+        }
+    }
+
+    private View getTeacherView(Teacher teacher, LayoutInflater inflater) {
+        if (inflater == null) {
+            inflater = LayoutInflater.from(getActivity());
+        }
+        View teacherView = inflater.inflate(R.layout.item_teacher, mTeachersLinearLayout, false);
+        TextView teacherName = (TextView) teacherView.findViewById(R.id.item_teacher_tv_teacher_name);
+        TextView teacherDescribe = (TextView) teacherView.findViewById(R.id.item_teacher_tv_teacher_describe);
+        teacherName.setText(teacher.getName());
+        teacherDescribe.setText(teacher.getPost());
+
+        return teacherView;
     }
 }
